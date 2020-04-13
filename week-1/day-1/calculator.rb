34fcc9877ad11795c65f9bbca2c3b332d3bdd3f9
Marvin Carrasco <marccenvivo@gmail.com>
2020-04-13 18:12:15 -0500

    solution calculator assignment

---
+++ week-1/day-1/calculator.rb
@@ -0,0 +1,25 @@
+class Calculator
+  def add(a, b)
+   return a + b
+  end
+
+  def subtract(a, b)
+   retun a - b
+  end
+   
+  def divide(a, b)
+   return a / b
+  end
+  
+  def multiply(a, b)
+    return a * b
+  end
+   
+  def exponentiate(a, b)
+    a ** b
+  end
+  
+  def modulo(a, b)
+    a % b
+  end
+end
