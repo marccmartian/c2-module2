0e077abc2a1f7f412a9036bff3e52c6380cf9e70
Marvin Carrasco <marccenvivo@gmail.com>
2020-04-13 22:41:53 -0500

    update calculator

--- week-1/day-1/calculator.rb
+++ week-1/day-1/calculator.rb
@@ -1,10 +1,10 @@
 class Calculator
   def add(a, b)
-   return a + b
+    return a + b
   end
 
   def subtract(a, b)
-   retun a - b
+   return a - b
   end
    
   def divide(a, b)
@@ -16,10 +16,19 @@ class Calculator
   end
    
   def exponentiate(a, b)
-    a ** b
+    return a ** b
   end
   
   def modulo(a, b)
-    a % b
+    return a % b
   end
 end
+
+# Usage, copy and paste this to make sure your program works correctly.
+calculator = Calculator.new
+puts "Sum result is #{calculator.add(5, 4)}" # Should output "Sum result is 9"
+puts "Subtraction result is #{calculator.subtract(5, 4)}" # Should output "Subtraction result is 1"
+puts "Division result is #{calculator.divide(12, 4)}" # Should output "Division result is 3"
+puts "Multiplication result is #{calculator.multiply(5, 4)}" # Should output "Multiplication result is 20"
+puts "Exponentiation result is #{calculator.exponentiate(2, 3)}" # Should output "Exponentiation result is 8"
+puts "Modulo result is #{calculator.modulo(5, 2)}" # Should output "Modulo result is 1"
